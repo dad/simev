d0986904e540d831109d2380517268c5626a3ebe
D. Allan Drummond <dadrummond@gmail.com>
2012-05-13 16:27:50 -0500

    starting to add analytical methods; all tests pass

--- analysis/wrightfisher.py
+++ analysis/wrightfisher.py
@@ -21,8 +21,10 @@ class SpawnResult:
 class Evolvable:
 	def __init__(self):
 		pass
+	
 	def fitness(self):
 		raise NotImplementedException, "Must override"
+	
 	def spawn(self, mutation_rate):
 		raise NotImplementedException, "Must override"
 
@@ -32,7 +34,11 @@ class MutationInfo:
 		self.from_base = from_base
 		self.to_base = to_base
 
-class SimpleMutator:
+class Mutator:
+	def mutate(self, sequence):
+		raise NotImplementedException, "Must override"
+
+class SimpleMutator(Mutator):
 	def __init__(self, per_site_mutation_rate, alphabet = 'ATGC'):
 		self.per_site_mutation_rate = per_site_mutation_rate
 		self.alphabet = alphabet
@@ -47,19 +53,22 @@ class SimpleMutator:
 				mut_sequence[l] = random.choice(self.mut_choices[mut_sequence[l]])
 				mutations.append(MutationInfo(l, from_base, mut_sequence[l]))
 		return ''.join(mut_sequence), mutations
-		
 
 class EvolvableSequence(Evolvable):
 	def __init__(self, sequence):
 		self.sequence = sequence
 
+	def copy(self):
+		e = EvolvableSequence(self.sequence)
+		return e
+
 	def fitness(self):
 		return 1.0
 
-	def spawn(self, mutator): #per_site_mutation_rate):
+	def spawn(self, mutator):
 		res = SpawnResult()
 		(mut_sequence, mutations) = mutator.mutate(self.sequence)
-		offs = EvolvableSequence(''.join(mut_sequence))
+		offs = EvolvableSequence(mut_sequence)
 		res.offspring = offs
 		res.mutations = mutations
 		return res
@@ -84,11 +93,14 @@ def pickIndexByProbability(cum_probs, p):
 # Track lineages. When they arise, start keeping a count of their frequencies; when they're extinguished
 # This is borrowed heavily from Claus's GeneBank.
 class GeneBankEntry:
-	def __init__(self, organism, parent, generations):
+	def __init__(self, organism, parent, fitness, birthtime):
 		self.organism = organism
 		self.parent = parent
-		self.generations = generations
-		self.refcount = 1
+		self.fitness = fitness
+		self.birthtime = birthtime
+		self.refcount = 0
+		self.coalescent = False
+		self._id = None
 
 	def addReference(self):
 		self.refcount += 1
@@ -97,76 +109,182 @@ class GeneBankEntry:
 	def removeReference(self):
 		self.refcount -= 1
 		return self.refcount
+
+	def setCoalescent(self, bool):
+		self.coalescent = bool
+
+	@property
+	def id(self, the_id):
+		self._id = the_id
+	
+	@property
+	def id(self):
+		return self._id
+
+	def __str__(self):
+		return "{} ({})".format(self.organism, self.refcount)
+
+	def __eq__(self, other):
+		# DAD: not quite sure how strict to be here
+		return self.organism == other.organism
 	
 class GeneBank:
 	def __init__(self):
+		# Table maps keys (IDs) to GeneBankEntries.
 		self.table = {}
+		# An ID counter. Increments with each entry created.
+		self.maxID = 0
+
+	# Factory model: GeneBank creates entries which are owned and distributed by it.
+	def createEntry(self, organism, parent_entry, fitness, birthtime):
+		entry = GeneBankEntry(organism, parent_entry, fitness, birthtime)
+		if not parent_entry is None:
+			parent_entry.addReference()
+		self.table[self.maxID] = entry
+		entry.id = self.maxID
+		self.maxID += 1
+		return entry
+
+	def getEntry(self, id):
+		return self.table.get(id)
 		
-	def add(self, item, generation):
-		key = item.key
-		try:
-			entry = self.table[key]
-			entry.addReference()
-		except KeyError, ke:
-			# DAD: implement
-			self.table[key] = 1
-			
+	def addEntry(self, entry):
+		entry.addReference()
 
-	def frequency(self, item):
-		return self.frequency_table[item.key()]
-
-class GeneBankAnalyzer:
-	def __init__(self):
-		pass
+	def removeEntry(self, entry):
+		if not entry is None:
+			if entry.removeReference()==0:
+				# Refcount is zero: time to remove the organism from our world.
+				del self.table[entry.id]
+				if not entry.parent is None:
+					self.removeEntry(entry.parent)
 
 class Population:
-	def __init__(self, mutator):
-		self.members = []
+	def __init__(self, population_size, mutator):
+		self.population_size = population_size
+		# List of members of the population. These are GeneBankEntry objects, which wrap organisms to allow reference-counting.
+		self._members = []
+		# A buffer for building subsequent generations of the population.
+		self._members_buffer = []
+		# Class supporting the .mutate() method.
 		self.mutator = mutator
+		# The gene bank: the set of organisms along the line of descent.
 		self.genebank = GeneBank()
+		# The number of generations since the beginning of the simulation.
 		self.generation_count = 0
 
-	def addMember(self, member):
-		self.genebank.add(member, self.generations)
-		self.members.append(member)
+	def addMember(self, entry):
+		# Add to the population
+		self._members.append(entry)
+
+	def createOffspring(self, organism_entry, mutate=True):
+		"""Creates but does not add offspring."""
+		# Begin assuming no mutation will occur
+		new_entry = organism_entry
+		if mutate:
+			# Reproduce with mutation
+			spawnres = organism_entry.organism.spawn(self.mutator)
+			if spawnres.mutated:
+				# Create an entry for the GeneBank
+				new_entry = self.genebank.createEntry(spawnres.offspring, organism_entry, spawnres.offspring.fitness(), self.generation_count)
+		# Add to the GeneBank
+		self.genebank.addEntry(new_entry)
+		return new_entry
+
+	def populate(self, organism):
+		"""Add population_size copies of the specified organism"""
+		# Make a parent for the whole population
+		parent = organism
+		parent_entry = self.genebank.createEntry(parent, None, parent.fitness(), self.generation_count)
+		parent_entry.setCoalescent(True)
+		# All new population
+		for i in range(self.population_size):
+			self.addMember(self.createOffspring(parent_entry, mutate=False))
+		# Remove the parent -- it's no longer in the population
+		self.genebank.removeEntry(parent_entry)
 
 	def makeCumulativeProbabilities(self):
 		total_fitness = 0.0
-		for m in self.members:
-			m.cache_fitness = m.fitness()
+		for m in self._members:
+			m.cache_fitness = m.organism.fitness()
 			total_fitness += m.cache_fitness
-		self.members.sort(key = lambda x: x.cache_fitness, reverse=True)
+		self._members.sort(key = lambda x: x.cache_fitness, reverse=True)
 		cum_probs = []
 		cum_prob = 0.0
-		for m in self.members:
+		for m in self._members:
 			cum_prob += m.cache_fitness/total_fitness
 			cum_probs.append(cum_prob)
 		return cum_probs
 
 	def evolve(self, num_generations):
 		for n in xrange(num_generations):
-			nextgen = []
+			# Clear out the buffer
+			self._members_buffer = []
 			cum_probs = self.makeCumulativeProbabilities()
 			# Increment generations
 			self.generation_count += 1
 			# Replicate into the new generation
-			for nm in xrange(len(self.members)):
+			for nm in xrange(len(self._members)):
 				# Pick parent according to fitness: Wright-Fisher sampling
-				parent = self.members[pickIndexByProbability(cum_probs, random.random())]
+				parent_entry = self._members[pickIndexByProbability(cum_probs, random.random())]
 				# Reproduce with mutation
-				spawn_result = parent.spawn(self.mutator)
+				spawn_entry = self.createOffspring(parent_entry)
 				# Insert offspring into new gen
-				nextgen.append(spawn_result.offspring)
+				self._members_buffer.append(spawn_entry)
 				# Track
 				#self.genebank.add(spawn_result, self.generations)
-			# Replace population
-			self.members = nextgen
+			# Replace population with the buffer
+			self._members = self._members_buffer
+
+	def erase(self):
+		"""Get rid of all information in the population."""
+		self._members = []
+		self.genebank.erase()
+
+	## Analytical methods
+	def frequency(self, entry):
+		"""Get the frequency of a particular organism."""
+		return self.table[item.key()]
+
+	def histogram(self):
+		"""Return a sorted count of the number of each type of organism in the population"""
+		ids = [e.id for e in self._members]
+		hist = []
+		for id in set(ids):
+			hist.append(ids.count(id))
+		return sorted(hist, reverse=True)
+
+	def dominantOrganism(self):
+		ids = [e.id for e in self._members]
+		id_counts = sorted([(ids.count(id),id) for id in ids], reverse=True)
+		return self.genebank.getEntry(id_counts[0][1])
+		
+	def averageFitness(self):
+		total_fitness = 0.0
+		for m in self._members:
+			m.cache_fitness = m.organism.fitness()
+			total_fitness += m.cache_fitness
+		return total_fitness/self.size()
 				
 	@property
 	def size(self):
-		return len(self.members)
+		assert len(self._members) == self.population_size
+		return self.population_size
 
 	@property
 	def generations(self):
 		return self.generation_count
+
+	@property
+	def members(self):
+		for m in self._members:
+			yield m
+
+	def count(self, entry):
+		ids = [e.id for e in self._members]
+		return ids.count(entry.id)
 		
+		
+	def __str__(self):
+		for m in self._members:
+			print m
--- analysis/wrightfisher_test.py
+++ analysis/wrightfisher_test.py
@@ -2,16 +2,18 @@ import sys, os, math, string, random, unittest
 import stats
 import wrightfisher as wf
 
-class Organism(wf.Evolvable):
+class TraitOrganism(wf.Evolvable):
 	def __init__(self):
 		self.trait = 1.0
 		self.key = randomSequence(12,string.letters)
+
 	def fitness(self):
 		return self.trait
+	
 	def spawn(self, mutation_rate):
 		res = wf.SpawnResult()
 		p = random.random()
-		offs = Organism()
+		offs = TraitOrganism()
 		res.offspring = offs
 		if p < mutation_rate:
 			offs.trait = self.trait + random.random()
@@ -30,9 +32,8 @@ class test001(unittest.TestCase):
 	"""basic run"""
 	def test_run(self):
 		alphabet='ATGC'
-		p = wf.Population(wf.SimpleMutator(0.01,alphabet))
-		for n in range(10):
-			p.addMember(Organism())
+		p = wf.Population(10, wf.SimpleMutator(0.01,alphabet))
+		p.populate(TraitOrganism())
 		p.evolve(1000)
 		#print len(p.members)
 		#for o in p.members:
@@ -57,9 +58,8 @@ class test003(unittest.TestCase):
 	"""Sequence"""
 	def test_run(self):
 		alphabet = 'ATGC'
-		p = wf.Population(wf.SimpleMutator(0.01,alphabet))
-		for n in range(10):
-			p.addMember(wf.EvolvableSequence(randomSequence(20,alphabet)))
+		p = wf.Population(10, wf.SimpleMutator(0.01,alphabet))
+		p.populate(wf.EvolvableSequence(randomSequence(20,alphabet)))
 		p.evolve(1000)
 		#for m in p.members:
 		#	print m
@@ -67,8 +67,50 @@ class test003(unittest.TestCase):
 class test004(unittest.TestCase):
 	def test_run(self):
 		e = wf.EvolvableSequence(randomSequence(100,'ATGC'))
+		# Zero mutation rate copy
 		off = e.spawn(wf.SimpleMutator(0.0)).offspring
+		# Should yield identical sequences
 		assert e == off
+
+class test005(unittest.TestCase):
+	"""Tracking frequency"""
+	def test_run(self):
+		alphabet = 'ATGC'
+		pop = wf.Population(100,wf.SimpleMutator(0.0001,alphabet))
+		seq = wf.EvolvableSequence(randomSequence(100,'ATGC'))
+		pop.populate(seq)
+		pop.evolve(100)
+		dom_seq_entry = pop.dominantOrganism()
+		counts = []
+		for m in pop.members:
+			counts.append((pop.count(m),m))
+		counts.sort(reverse=True)
+		assert dom_seq_entry == counts[0][1]
+
+class test006(unittest.TestCase):
+	"""Histogram"""
+	def test_run(self):
+		alphabet = 'ATGC'
+		n = 1000
+		pop = wf.Population(n,wf.SimpleMutator(0.0001,alphabet))
+		seq = wf.EvolvableSequence(randomSequence(90,'ATGC'))
+		pop.populate(seq)
+		h = pop.histogram()
+		assert h[0] == n
+		for i in range(10):
+			pop.evolve(1)
+			h = pop.histogram()
+			assert sum(h) == n
+
+class test007(unittest.TestCase):
+	"""Members iterator"""
+	def test_run(self):
+		alphabet = 'ATGC'
+		n = 1000
+		pop = wf.Population(n,wf.SimpleMutator(0.0001,alphabet))
+		seq = wf.EvolvableSequence(randomSequence(90,'ATGC'))
+		pop.populate(seq)
+		
 
 if __name__=="__main__":
 	unittest.main(verbosity=2)
