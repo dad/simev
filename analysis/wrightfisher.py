8c3183b400d606f4cf5677e80351bd5b3c1551c1
D. Allan Drummond <dadrummond@gmail.com>
2012-05-13 09:27:44 -0500

    evolvable sequence working

--- analysis/wrightfisher.py
+++ analysis/wrightfisher.py
@@ -1,4 +1,5 @@
 import sys, os, math, string, random, unittest
+import stats
 
 class NotImplementedException(Exception):
 	def __init__(self):
@@ -16,6 +17,32 @@ class Evolvable:
 		raise NotImplementedException, "Must override"
 	def spawn(self, mutation_rate):
 		raise NotImplementedException, "Must override"
+
+class EvolvableSequence(Evolvable):
+	def __init__(self, sequence, alphabet = 'ATGC'):
+		self.sequence = sequence
+		self.alphabet = alphabet
+		self.mut_choices = dict([(x,alphabet.replace(x,'')) for x in alphabet])
+
+	def fitness(self):
+		return 1.0
+
+	def spawn(self, per_site_mutation_rate):
+		res = SpawnResult()
+		mut_sequence = [x for x in self.sequence]
+		for l in range(len(mut_sequence)):
+			if random.random() < per_site_mutation_rate:
+				mut_sequence[l] = random.choice(self.mut_choices[mut_sequence[l]])
+				res.mutated = True
+		offs = EvolvableSequence(''.join(mut_sequence), self.alphabet)
+		res.offspring = offs
+		return res
+
+	def __str__(self):
+		return self.sequence
+
+	def eq(self,x):
+		return self.sequence == x.sequence
 	
 def pickIndexByProbability(cum_probs, p):
 	assert p>=0.0 and p<=1.0
@@ -24,27 +51,43 @@ def pickIndexByProbability(cum_probs, p):
 		i+=1
 	return i
 
+class LineageTracker:
+	def __init__(self):
+		self.frequency_table = {}
+	def add(self, item, generation):
+		pass
+
 class Population:
-	def __init__(self, num_members, member_type, mutation_rate):
+	def __init__(self, mutation_rate):
 		self.members = []
-		for n in xrange(num_members):
-			m = member_type()
-			self.members.append(m)
 		self.mutation_rate = mutation_rate
+		self.tracker = LineageTracker()
+		self.generation_count = 0
+
+	def addMember(self, member):
+		self.tracker.add(member, self.generations)
+		self.members.append(member)
+
+	def makeCumulativeProbabilities(self):
+		total_fitness = 0.0
+		for m in self.members:
+			m.cache_fitness = m.fitness()
+			total_fitness += m.cache_fitness
+		self.members.sort(key = lambda x: x.cache_fitness, reverse=True)
+		cum_probs = []
+		cum_prob = 0.0
+		for m in self.members:
+			cum_prob += m.cache_fitness/total_fitness
+			cum_probs.append(cum_prob)
+		return cum_probs
 
 	def evolve(self, num_generations):
 		for n in xrange(num_generations):
 			nextgen = []
-			total_fitness = 0.0
-			for m in self.members:
-				m.cache_fitness = m.fitness()
-				total_fitness += m.cache_fitness
-			self.members.sort(key = lambda x: x.cache_fitness, reverse=True)
-			cum_probs = []
-			cum_prob = 0.0
-			for m in self.members:
-				cum_prob += m.cache_fitness/total_fitness
-				cum_probs.append(cum_prob)
+			cum_probs = self.makeCumulativeProbabilities()
+			# Increment generations
+			self.generation_count += 1
+			# Replicate into the new generation
 			for nm in xrange(len(self.members)):
 				# Pick parent according to fitness
 				parent = self.members[pickIndexByProbability(cum_probs, random.random())]
@@ -52,10 +95,16 @@ class Population:
 				spawn_result = parent.spawn(self.mutation_rate)
 				# Insert offspring into new gen
 				nextgen.append(spawn_result.offspring)
+				# Track
+				self.tracker.add(spawn_result, self.generations)
 			# Replace population
 			self.members = nextgen
-			# Increment
 				
-		
-	
+	@property
+	def size(self):
+		return len(self.members)
+
+	@property
+	def generations(self):
+		return self.generation_count
 		
--- analysis/wrightfisher_test.py
+++ analysis/wrightfisher_test.py
@@ -1,4 +1,5 @@
 import sys, os, math, string, random, unittest
+import stats
 import wrightfisher as wf
 
 class Organism(wf.Evolvable):
@@ -13,7 +14,7 @@ class Organism(wf.Evolvable):
 		res.offspring = offs
 		if p < mutation_rate:
 			offs.trait = self.trait + random.random()
-			wf.mutated = True
+			res.mutated = True
 		else:
 			offs.trait = self.trait
 		return res
@@ -21,11 +22,43 @@ class Organism(wf.Evolvable):
 class test001(unittest.TestCase):
 	"""basic run"""
 	def test_run(self):
-		print "here"
-		p = wf.Population(10, Organism, 0.01)
+		p = wf.Population(0.01)
+		for n in range(10):
+			p.addMember(Organism())
 		p.evolve(1000)
+		print len(p.members)
 		for o in p.members:
 			print o.trait
+
+class test002(unittest.TestCase):
+	"""pickByIndex"""
+	def test_run(self):
+		p = wf.Population(0.01)
+		for n in range(10):
+			p.addMember(Organism())
+		p.evolve(1)
+		for o in p.members:
+			print o.trait
+
+def randomSequence(n, alphabet):
+	return ''.join(stats.sample_wr(alphabet, n))
+
+class test003(unittest.TestCase):
+	"""Sequence"""
+	def test_run(self):
+		p = wf.Population(0.01)
+		alphabet = 'ATGC'
+		for n in range(10):
+			p.addMember(wf.EvolvableSequence(randomSequence(20,alphabet),alphabet))
+		p.evolve(1000)
+
+class test004(unittest.TestCase):
+	def test_run(self):
+		e = wf.EvolvableSequence(randomSequence(10,'ATGC'),'ATGC')
+		off = e.spawn(0.0).offspring
+		print e
+		print off
+		assert e.eq(off)
 
 if __name__=="__main__":
 	unittest.main(verbosity=2)
