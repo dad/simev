fb996ef3caba0a1091a6749306d98c55fdb37c7e
D. Allan Drummond <dadrummond@gmail.com>
2012-05-13 09:54:05 -0500

    pickIndex test

--- analysis/wrightfisher_test.py
+++ analysis/wrightfisher_test.py
@@ -19,6 +19,12 @@ class Organism(wf.Evolvable):
 			offs.trait = self.trait
 		return res
 
+def randomSequence(n, alphabet):
+	return ''.join(stats.sample_wr(alphabet, n))
+
+
+## TEST CASES
+
 class test001(unittest.TestCase):
 	"""basic run"""
 	def test_run(self):
@@ -26,22 +32,24 @@ class test001(unittest.TestCase):
 		for n in range(10):
 			p.addMember(Organism())
 		p.evolve(1000)
-		print len(p.members)
-		for o in p.members:
-			print o.trait
+		#print len(p.members)
+		#for o in p.members:
+		#	print o.trait
 
 class test002(unittest.TestCase):
 	"""pickByIndex"""
 	def test_run(self):
-		p = wf.Population(0.01)
-		for n in range(10):
-			p.addMember(Organism())
-		p.evolve(1)
-		for o in p.members:
-			print o.trait
-
-def randomSequence(n, alphabet):
-	return ''.join(stats.sample_wr(alphabet, n))
+		n = 100
+		v = range(n)
+		sv = sum(v)
+		# Set up cumulative probabilities that we know for certain.
+		cum_probs = []
+		for x in v:
+			cum_probs.append(x/float(n))
+		# Now know that
+		for x in range(n):
+			#print x, wf.pickIndexByProbability(cum_probs, x/float(n))
+			assert wf.pickIndexByProbability(cum_probs, x/float(n)) == x
 
 class test003(unittest.TestCase):
 	"""Sequence"""
@@ -51,13 +59,13 @@ class test003(unittest.TestCase):
 		for n in range(10):
 			p.addMember(wf.EvolvableSequence(randomSequence(20,alphabet),alphabet))
 		p.evolve(1000)
+		#for m in p.members:
+		#	print m
 
 class test004(unittest.TestCase):
 	def test_run(self):
-		e = wf.EvolvableSequence(randomSequence(10,'ATGC'),'ATGC')
+		e = wf.EvolvableSequence(randomSequence(100,'ATGC'),'ATGC')
 		off = e.spawn(0.0).offspring
-		print e
-		print off
 		assert e.eq(off)
 
 if __name__=="__main__":
